16403eac5994a590fae5b89f261b2c1cf696e4d3
Brayan Maldonado <bmaldonadoa@unal.edu.co>
2024-06-09 19:57:55 -0500

    Cambios vista About

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -11,7 +11,11 @@ export default function About(){
         <img src="Natu_Logo_.png" id="img-centrada2" />
     </div>
     <h2 id='titulo2'>Acerca de nosotros</h2>
-      <p className='vision'>Natu nace como alternativa para el encuentro rápido de alimentos orgánicos y de producción local en un solo lugar. El objetivo principal es conectar productores locales de alimentos de origen natural con consumidores conscientes por medio de una plataforma en línea que permita una transacción rápida y una trazabilidad del estado de entrega del producto por parte del cliente. La plataforma no solo se propone simplificar la adquisición de productos saludables y sostenibles, sino también busca ser de apoyo para el crecimiento de agricultores y campesinos colombianos. Más que ofrecer una experiencia de compra, se quiere fomentar un estilo de vida saludable y consciente, respaldando a aquellos productores que trabajan para cultivar y producir alimentos nutritivos y de calidad. Contribuyendo así al bienestar tanto al consumidor final como al primer eslabón dentro de la cadena de consumo de estos productos, esto por medio de la adaptabilidad, la colaboración, la mejora continua y la calidad en nuestros servicios.
+      <p className='vision'> Natu surge como una alternativa para facilitar el acceso a productos orgánicos y de producción local en un solo lugar. Su objetivo principal es conectar a los productores locales con los consumidores a través de una plataforma en línea que permite transacciones rápidas y eficientes. Esta plataforma no solo simplifica la adquisición de productos saludables y sostenibles, sino que también apoya el crecimiento de agricultores y campesinos colombianos.
+
+Más allá de ofrecer una experiencia de compra, Natu promueve un estilo de vida saludable y consciente, respaldando a aquellos productores que se dedican a cultivar y producir alimentos nutritivos y de alta calidad. De esta manera, contribuye al bienestar tanto del consumidor final como de los productores, el primer eslabón en la cadena de consumo.
+
+Natu se basa en la adaptabilidad, la colaboración, la mejora continua y la calidad de sus servicios para cumplir su misión.
 </p>
     <h2 id='titulo2'>Visión</h2>
       <p className='vision'>Para 2028, Natu se posicionará como la principal plataforma de intercambio de productos orgánicos en Colombia, logrando la colaboración activa entre productores y empresas dedicadas a alimentos orgánicos con el fin de aumentar el alcance de compra por parte de los consumidores de nuestro país priorizando la calidad del servicio y adaptándonos a las cambiantes necesidades del mercado. Buscamos ser una herramienta integral que involucre alrededor del 30% de los actores del sector orgánico, proporcionando a los usuarios una experiencia de alta calidad y satisfaciendo sus expectativas en constante evolución.
@@ -28,7 +32,7 @@ export default function About(){
         <div id="team-member2">
         <img src="https://media.licdn.com/dms/image/D4E03AQGWNZAEWFe67A/profile-displayphoto-shrink_400_400/0/1690652960251?e=1718841600&v=beta&t=bQZTsqiEyCEPuB5acAngChDqAPTLO2713XAqncR1x-U" alt="Integrante 1" />
           <h3 >David Rodriguez</h3>
-          <p>Desarrollador Backend</p>
+          <p>Desarrollador Full-Stack</p>
           <a href="https://github.com/Davidrg02" target="_blank" rel="noreferrer" className='giro' style={{ fontSize: '1.8em' }} >
             <i className="fab fa-github"></i>
           </a>
